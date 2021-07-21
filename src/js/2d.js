b919698369b2777793c7cc9662c92646d799660f
johnmogi <anguru@gmail.com>
2021-07-21 07:12:04 +0300

    --drawing

---
+++ src/js/2d.js
@@ -0,0 +1,7 @@
+let x = '[],[],[]';
+
+for (let i = 0; i < 3; i++) {
+    console.log(x);
+    
+    
+}
--- src/js/index.js
+++ src/js/index.js
@@ -8,42 +8,33 @@ const oneDimensionArr = ['a','b','c','d','e','f','g','h','i'];
 const twoDimensionArr = ['1','2','3','4','5','6','7','8','9'];
 
 function drawBoard(){
-    const ctx = myBoard.getContext("2d");
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(0, 300);
-    ctx.lineTo(300, 300);
-    ctx.lineTo(300, 0);
-    ctx.lineTo(0, 0);
+    const divElement = document.createElement("div");
+    myBoard.appendChild(divElement);
+    divElement.classList.add('row');
 
-    ctx.moveTo(0, 30);
-    ctx.lineTo(300, 30);
+    for (let i = 0; i < 3; i++) {
+    let div = document.createElement("div")
+    div.classList.add('card.col');
+    divElement.appendChild(div);
+      
+    for (let j = 0; j < 3; j++) {
+        let divj = document.createElement("div")
+        divj.classList.add('card.col');
+        divElement.appendChild(divj);
+            
+        }
+    }
 
-    ctx.moveTo(0, 60);
-    ctx.lineTo(300, 60); 
- 
-    ctx.moveTo(0, 90);
-    ctx.lineTo(300, 90);
 
-    ctx.moveTo(0, 120);
-    ctx.lineTo(300, 120);
+
+//     let div = "<div>"
+//     let x ='</div>';
+//     console.log(myBoard);
     
-    ctx.moveTo(0, 150);
-    ctx.lineTo(300, 150);
-    
-    ctx.moveTo(0, 180);
-    ctx.lineTo(300, 180);
-
-    ctx.moveTo(0, 210);
-    ctx.lineTo(300, 210);
-
-    ctx.moveTo(0, 240);
-    ctx.lineTo(300, 240);
-
-    ctx.moveTo(0, 270);
-    ctx.lineTo(300, 270);
-    
-    ctx.stroke();
+// for (let i = 0; i < 3; i++) {
+//     myBoard.appendChild(div, x);  
+// }
+// myBoard.appendChild(x);
 
 }
 
